d83862b4a57d144eed4be55855441524abbad080
Ihor kulpekin <konstantin.boykov@pro-store.online>
2020-12-12 17:56:34 +0200

    change src

--- atoms/Logo.tsx
+++ atoms/Logo.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import logo from '../assets/logo.png';
-
 const WrapperLogo = styled.div`
-  .logo{
-    object-fit: cover;
-    width: 160px;
-    height: 50px;
-    margin: 0;
-    padding: 0;
-  }
-`
+    .logo {
+        object-fit: cover;
+        width: 160px;
+        height: 50px;
+        margin: 0;
+        padding: 0;
+    }
+`;
 
 const Logo = () => {
     return (
         <WrapperLogo>
-            <img src={logo} className="logo" alt="logo" />
+            <img src="../assets/logo.png" className="logo" alt="logo" />
         </WrapperLogo>
-    )
-}
+    );
+};
 
 export default Logo;
